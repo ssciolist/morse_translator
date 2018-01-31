91a9fe2fb94363e4a6439328337230761632cdc5
Megan Arellano <ssciolist@gmail.com>
2018-01-31 09:28:56 -0700

    Adds translator with it_exists test

--- test/translator_test.rb
+++ test/translator_test.rb
@@ -1,1 +1,12 @@
-# YOUR TESTS GO IN HERE!
+require "minitest"
+require "minitest/pride"
+require "minitest/autorun"
+require "./lib/translator"
+
+class Test_Translator < Minitest::Test
+
+  def test_translator_exists
+    translator = Translator.new
+    assert_instance_of Translator, translator
+  end
+end
